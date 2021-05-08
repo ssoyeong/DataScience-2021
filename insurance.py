6a325697203e47576942baa757319d7fdffeac66
yedin123 <73570623+yedin123@users.noreply.github.com>
2021-05-08 15:14:36 +0900

    Update insurance.py

--- insurance.py
+++ insurance.py
@@ -1,1 +1,8 @@
+import numpy as np
+import pandas as pd
+# read csv
+df = pd.read_excel('IT_3.xlsx')
+df = df.drop(['Age_bucket','EngineHP_bucket','Years_Experience_bucket','Miles_driven_annually_bucket','credit_history_bucket'], axis=1)
+df.fillna(df.mean(), inplace=True)
+df.fillna(axis=0, method='ffill',inplace=True)
 
