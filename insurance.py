0c5606faf1c0d4fb06b6be386f6da9223222af98
Hyojinko <62235186+Hyojinko@users.noreply.github.com>
2021-05-08 15:12:05 +0900

    Update insurance.py

--- insurance.py
+++ insurance.py
@@ -1,8 +1,1 @@
-import pandas as pd
-import numpy as np
-import seaborn as sns
-import matplot.pyplot as plt
 
-df = pd.read_excel('/Users/kohyojin/Desktop/데스크탑/SoftWare/software2021/data science/term_project/IT_3.xlsx')
-df=df.drop(['Age_bucket','EngineHP_bucket','Years_Experience_bucket','Miles_driven_annually_bucket'])
-df = df.fillna(method='ffill')
