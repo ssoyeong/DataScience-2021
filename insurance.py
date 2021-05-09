f4871a6462de0ac8c63d29798f871e6d3fccd2af
yedin123 <73570623+yedin123@users.noreply.github.com>
2021-05-09 18:11:58 +0900

    Update insurance.py

--- insurance.py
+++ insurance.py
@@ -15,85 +15,78 @@ df.fillna(df.mean(), inplace=True)
 df.fillna(axis=0, method='ffill',inplace=True)
 # print(df.isnull().sum())
 
+df_ordinal = df.copy()
+df_oneHot = df.copy()
+df_label = df.copy()
+
+# Convert 'Marital_Status' feature to numeric values using ordinalEncoder
+ordinalEncoder = preprocessing.OrdinalEncoder()
+X = pd.DataFrame(df['Marital_Status'])
+ordinalEncoder.fit(X)
+df_ordinal['Marital_Status'] = pd.DataFrame(ordinalEncoder.transform(X))
+
+# Convert 'Marital_Status' feature to numeric values using oneHotEncoder
+oneHotEncoder = preprocessing.OneHotEncoder()
+oneHotEncoder.fit(X)
+df_oneHot['Marital_Status'] = pd.DataFrame(oneHotEncoder.transform(X).toarray())
+
+# Convert 'Marital_Status' feature to numeric values using labelEncoder
+labelEncoder = preprocessing.LabelEncoder()
+labelEncoder.fit(df_label['Marital_Status'])
+df_label['Marital_Status'] = labelEncoder.transform(df_label['Marital_Status'])
+
 
 #Using OrdinalEncoder
 ordEnc = preprocessing.OrdinalEncoder()
 X = pd.DataFrame(df['Vehical_type'])
 ordEnc.fit(X)
-ordVeh = pd.DataFrame(ordEnc.transform(X))
-# print(ordVeh.head())
+df_ordinal['Vehical_type'] = pd.DataFrame(ordEnc.transform(X))
+
 #Using OneHotEncoder
 onehotEnc=preprocessing.OneHotEncoder()
 onehotEnc.fit(X)
-oneHotVeh = pd.DataFrame(onehotEnc.transform(X).toarray())
-print(oneHotVeh.head())
+df_oneHot['Vehical_type'] = pd.DataFrame(onehotEnc.transform(X).toarray())
+
 #Using LabelEncoder
 labelEnc = preprocessing.LabelEncoder()
 labelEnc.fit(df['Vehical_type'])
-labelVeh = pd.DataFrame(labelEnc.transform(df['Vehical_type']))
-print(labelVeh.head())
+df_label['Vehical_type'] = pd.DataFrame(labelEnc.transform(df['Vehical_type']))
 
-
-# Convert 'Marital_Status' feature to numeric values using ordinalEncoder
-df_ordinal = df.copy()
-ordinalEncoder = preprocessing.OrdinalEncoder()
-X = pd.DataFrame(df['Marital_Status'])
-ordinalEncoder.fit(X)
-df_ordinal['Marital_Status'] = pd.DataFrame(ordinalEncoder.transform(X))
-print(df_ordinal.head(20))
-
-# Convert 'Marital_Status' feature to numeric values using oneHotEncoder
-df_oneHot = df.copy()
-oneHotEncoder = preprocessing.OneHotEncoder()
-oneHotEncoder.fit(X)
-df_oneHot['Marital_Status'] = pd.DataFrame(oneHotEncoder.transform(X).toarray())
-print(df_oneHot.head(20))
-
-# Convert 'Marital_Status' feature to numeric values using labelEncoder
-df_label = df.copy()
-labelEncoder = preprocessing.LabelEncoder()
-labelEncoder.fit(df_label['Marital_Status'])
-df_label['Marital_Status'] = labelEncoder.transform(df_label['Marital_Status'])
-print(df_label.head(20))
 
 
 # Convert 'Gender' features to numeric values using ordinalEncoder
 X = pd.DataFrame(df['Gender'])
 ordEnc.fit(X)
-ordGen  = pd.DataFrame(ordEnc.transform(X))
-print(ordGen.head(10))
+df_ordinal['Gender']  = pd.DataFrame(ordEnc.transform(X))
 
 # Convert 'Gender' features to numeric values using onehotEncoder
 onehotEnc.fit(X)
-oneHotGen = pd.DataFrame(onehotEnc.transform(X))
-print(oneHotGen.head(10))
+df_oneHot['Gender'] = pd.DataFrame(onehotEnc.transform(X))
 
 # Convert 'Gender' features to numeric values using labelEncoder
 labelEnc.fit(X)
-labelGen = pd.DataFrame(labelEnc.transform(X))
-print(labelGen.head(10))
+df_label['Gender'] = pd.DataFrame(labelEnc.transform(X))
 
 
 # Convert 'State' features to numeric values using ordinalEncoder
 X = pd.DataFrame(df['State'])
 ordEnc.fit(X)
-ordState = pd.DataFrame(ordEnc.transform(X))
-print(ordState.head(10))
+df_ordinal['State'] = pd.DataFrame(ordEnc.transform(X))
+
 # Convert 'State' features to numeric values using onehotEncorder
 onehotEnc.fit(X)
-oneHotState = pd.DataFrame(onehotEnc.transform(X).toarray())
-print(oneHotState.head(10))
+df_oneHot['State'] = pd.DataFrame(onehotEnc.transform(X).toarray())
+
 # Convert 'State' features to numeric values using labelEncoder
 labelEnc.fit(df['State'])
-labelState = pd.DataFrame(labelEnc.transform(df['State']))
-print(labelState.head(10))
+df_label['State'] = pd.DataFrame(labelEnc.transform(df['State']))
 
-# 모든 categorical features를 encoding한 게 아니라 아직 안돌아갑니당
-# # Normalizing the ordinalEncoded dataset using MaxAbsScaler
-# scaler = preprocessing.MaxAbsScaler()
-# df_ordinal_maxAbs = scaler.fit_transform(df_ordinal)
-# df_ordinal_maxAbs = pd.DataFrame(df_ordinal_maxAbs, columns=df_ordinal.columns)
-# print(df_ordinal_maxAbs.head(10))
+
+# Normalizing the ordinalEncoded dataset using MaxAbsScaler
+scaler = preprocessing.MaxAbsScaler()
+df_ordinal_maxAbs = scaler.fit_transform(df_ordinal)
+df_ordinal_maxAbs = pd.DataFrame(df_ordinal_maxAbs, columns=df_ordinal.columns)
+print(df_ordinal_maxAbs.head(10))
 
 # # Normalizing the oneHotEncoded dataset using MaxAbsScaler
 # scaler = preprocessing.MaxAbsScaler()
@@ -101,8 +94,27 @@ print(labelState.head(10))
 # df_oneHot_maxAbs = pd.DataFrame(df_oneHot_maxAbs, columns=df_oneHot.columns)
 # print(df_oneHot_maxAbs.head(10))
 
-# # Normalizing the labelEncoded dataset using MaxAbsScaler
-# scaler = preprocessing.MaxAbsScaler()
-# df_label_maxAbs = scaler.fit_transform(df_label)
-# df_label_maxAbs = pd.DataFrame(df_label_maxAbs, columns=df_label.columns)
-# print(df_label_maxAbs.head(10))
+# Normalizing the labelEncoded dataset using MaxAbsScaler
+scaler = preprocessing.MaxAbsScaler()
+df_label_maxAbs = scaler.fit_transform(df_label)
+df_label_maxAbs = pd.DataFrame(df_label_maxAbs, columns=df_label.columns)
+print(df_label_maxAbs.head(10))
+
+
+# Normalizing the ordinalEncoded dataset using RobustScaler
+scaler = preprocessing.RobustScaler()
+df_ordinal_robust = scaler.fit_transform(df_ordinal)
+df_ordinal_robust = pd.DataFrame(df_ordinal_maxAbs, columns=df_ordinal.columns)
+print(df_ordinal_robust.head(10))
+
+# # Normalizing the oneHotEncoded dataset using RobustScaler
+# scaler = preprocessing.RobustScaler()
+# df_oneHot_robust = scaler.fit_transform(df_oneHot)
+# df_oneHot_robust = pd.DataFrame(df_oneHot_maxAbs, columns=df_oneHot.columns)
+# print(df_oneHot_robust.head(10))
+
+# Normalizing the labelEncoded dataset using RobustScaler
+scaler = preprocessing.RobustScaler()
+df_label_robust = scaler.fit_transform(df_label)
+df_label_robust = pd.DataFrame(df_label_maxAbs, columns=df_label.columns)
+print(df_label_robust.head(10))
