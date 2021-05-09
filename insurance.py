8f9f193fd94a148d1871ca896996e2925cba990b
Hyojinko <62235186+Hyojinko@users.noreply.github.com>
2021-05-09 11:36:07 +0900

    Update insurance.py

--- insurance.py
+++ insurance.py
@@ -6,3 +6,19 @@ df = df.drop(['Age_bucket','EngineHP_bucket','Years_Experience_bucket','Miles_dr
 df.fillna(df.mean(), inplace=True)
 df.fillna(axis=0, method='ffill',inplace=True)
 
+#Using OrdinalEncoder
+ordEnc = preprocessing.OrdinalEncoder()
+X = pd.DataFrame(df['Vehical_type'])
+ordEnc.fit(X)
+ordVeh = pd.DataFrame(ordEnc.transform(X))
+print(ordVeh.head())
+#Using OneHotEncoder
+onehotEnc=preprocessing.OneHotEncoder()
+onehotEnc.fit(X)
+oneHotVeh = pd.DataFrame(oneHotVeh.transform(X).toarray())
+print(oneHotEncoded.head())
+#Using LabelEncoder
+labelEnc = preprocessing.LabelEncoder()
+labelEnc.fit(df['Vehical_type'])
+labelVeh = pd.DataFrame(labelEnc.transform(df['Vehical_type']))
+print(labelVeh.head())
